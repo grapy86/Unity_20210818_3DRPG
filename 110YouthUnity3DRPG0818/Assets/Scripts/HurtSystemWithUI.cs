608cf2d6ebe2769a4833535fbb7fc4dd563496e9
Coffeeza <89112607+grapy86@users.noreply.github.com>
2021-11-24 12:00:08 +0800

    PC & NPC Attack System

--- 110YouthUnity3DRPG0818/Assets/Scripts/HurtSystemWithUI.cs
+++ 110YouthUnity3DRPG0818/Assets/Scripts/HurtSystemWithUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,14 +13,33 @@ namespace coffee
         [Header("要更新的血條")]
         public Image imgHp;
 
+        /// <summary>
+        /// 血條效果專用的扣血前血量
+        /// </summary>
+        private float hpEffectOriginal;
+
         // 欲複寫父類別成員須使用 override
         public override void Hurt(float damage)
         {
+            hpEffectOriginal = hp;
+
             // 該成員的父類別基底(base).父類別內的內容
             // 此行代表該父類別的所有內容，若刪除此行則不會執行
             base.Hurt(damage);
+            StartCoroutine(HpBarEffect());
+        }
 
-            imgHp.fillAmount = hp / hpMax;
+        /// <summary>
+        /// 血條遞減效果
+        /// </summary>
+        private IEnumerator HpBarEffect()
+        {
+            while (hpEffectOriginal != hp)
+            {
+                hpEffectOriginal--;
+                imgHp.fillAmount = hpEffectOriginal / hpMax;
+                yield return new WaitForSeconds(0.01f);
+            }
         }
     }
 }
